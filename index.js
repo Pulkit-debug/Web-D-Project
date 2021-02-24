645ca9f5b2f7575871312acd5f9f4c198eff9086
Pulkit Verma <vpulkit567@gmail.com>
2021-02-24 12:03:04 +0530

    setted ejs

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 // use express router from routes folder index.
 app.use("/", require("./routes/index"));
 
+// setting up the view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error From Server: ${port}`);
